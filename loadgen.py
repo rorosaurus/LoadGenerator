fdd84a3a2688bc81af0121962d047d343056468f
Rory Hayes <roryohayes@gmail.com>
2013-01-25 15:35:25 -0600

    Conditional network usage, variable iteration sizes

--- loadgen.py
+++ loadgen.py
@@ -9,23 +9,27 @@ millis = int(round(time.time() * 1000))
 random.seed(millis)
 
 # Define some sites to visit
-sites = ["http://www.google.com/", "http://www.yahoo.com/", "http://www.facebook.com", "http://www.twitter.com"]
+sites = ["http://www.google.com", "http://www.yahoo.com", "http://www.facebook.com", "http://www.twitter.com"]
 
 # Run continuously
 while True:
 
 	# Ramp up some CPU cycles
-	for i in range(0,random.randint(5000,9999)):
-		for j in range(0,random.randint(5000,9999)):
+	isize = random.randint(5000,9999)
+	for i in range(0,isize):
+		jsize = random.randint(5000,9999)
+		for j in range(0,jsize):
 			load = i + j
 
-	# Make an http request
-	try:
-		url = sites[random.randint(0,len(sites)-1)]
-		load = urllib2.urlopen(url).read()
-	except urllib2.URLError as error:
-		load = "http request failed"
+	# Only use network 1/3 of the time
+	if random.randint(0,2) == 0:
+		# Make an http request
+		try:
+			url = sites[random.randint(0,len(sites)-1)]
+			load = urllib2.urlopen(url).read()
+		except urllib2.URLError as error:
+			load = "http request failed"
 
-	# Wait a random amount of time between 4 and 8 hours
-	length = random.randint(60*60*4,60*60*8)
+	# Wait a random amount of time between 4 and 12 hours
+	length = random.randint(60*60*4,60*60*12)
 	time.sleep(length)
